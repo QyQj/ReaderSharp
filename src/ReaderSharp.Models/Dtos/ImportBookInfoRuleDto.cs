65439eb126ae9be69378aa8eb2b0161761621385
QyQj <ejyimy@outlook.com>
2022-05-07 22:29:53 +0800

    创建书源导入DTO

---
+++ src/ReaderSharp.Models/Dtos/ImportBookInfoRuleDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReaderSharp.Models.Dtos
+{
+    public class ImportBookInfoRuleDto
+    {
+
+        public string BookName { get; set; }
+
+        public string Author { get; set; }
+
+        public string CoverUrl { get; set; }
+
+        public string Introduction { get; set; }
+
+        public string LastChapter { get; set; }
+    }
+}
